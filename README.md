14a2f8fa84698ccb194eff713a17582786b815ea
ezlosswm <ezlosswm@gmail.com>
2023-03-30 08:32:05 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# dealbreaker.github.io
-Deal Breaker is a simple red card game made with HTML, CSS &amp; Vanilla JS.
+# Deal Breaker
+Deal Breaker is a simple red card game made with HTML, CSS &amp; Vanilla JS. 
+
+[Play the game here!](https://deal-breaker.vercel.app/)
---
+++ script.js
@@ -0,0 +1,58 @@
+import dataJson from './deck.json' assert {type: 'json'};
+
+const dealCards = document.querySelector('.button_container');
+const perkOne = document.getElementById('perkOne');
+const perkTwo = document.getElementById('perkTwo');
+const dealBreaker = document.getElementById('dealBreaker');
+
+// generates a random number based on the length of the deck provided
+let randN = (total) =>  Math.floor(Math.random() * total.length);
+
+function genRedCard(dealBreaker) {
+  let rand = randN(dealBreaker);
+
+  return dealBreaker[rand].joke
+}
+
+function genWhiteCard(perk) {
+    let rand = randN(perk)
+
+    return perk[rand].joke
+}
+
+// let genRedCard = (dealBreaker) => {
+//     let rand = randN(dealBreaker)
+
+//     return dealBreaker[rand].joke
+// }
+
+// let genWhiteCard = (perk) => {
+//     let rand = randN(perk)
+
+//     return perk[rand].joke
+// }
+
+dealCards.addEventListener('click', function() {
+    perkOne.innerText = genWhiteCard(dataJson.perks)
+    perkTwo.innerText = genWhiteCard(dataJson.perks)
+    dealBreaker.innerText = genWhiteCard(dataJson.dealbreakers)
+})
+
+// Modal content
+const modal = document.querySelector(".modal");
+const btn = document.getElementById("how_to_play");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
